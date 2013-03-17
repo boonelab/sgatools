a81227e6f7f7a51a75873ede9953dcf4225ff891
Omar Wagih <omarwagih@Omar-Wagihs-MacBook-Pro.local>
2013-03-17 15:28:23 -0400

    updated dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -7,12 +7,10 @@ object ApplicationBuild extends Build {
     val appName         = "sgatools"
     val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      "commons-io" % "commons-io" % "2.4"
-    )
+    val appDependencies = Seq()
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here    //"commons-io" % "commons-io" % "2.4"   
     )
 
 }
